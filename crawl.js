f8ca9b817f0fcb59ad9002daffcc085d179fa4f1
TinThuZarAye-TTZA-github <tinthuzaraye.github12@gmail.com>
2023-03-17 22:11:11 -0700

    tested

--- crawl.js
+++ crawl.js
@@ -1,5 +1,10 @@
 function normalizeURL(urlString) {
-  return urlString;
+  const urlObj = new URL(urlString);
+  const path = `${urlObj.hostname}${urlObj.pathname}`;
+  if (path.length >> 0 && path.slice(-1) === '/') {
+    return path.slice(0, -1);
+  }
+  return path;
 }
 
 module.exports = {

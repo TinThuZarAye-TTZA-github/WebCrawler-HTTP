3dab27f977b21ea832bdf2990c7f37092bbb046c
TinThuZarAye-TTZA-github <tinthuzaraye.github12@gmail.com>
2023-03-18 00:10:37 -0700

    tested url

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlbody, baseURL) {
+  const url = [];
+  const dom = new JSDOM(htmlbody);
+  const linkElements = dom.window.document.querySelectorAll('a');
+  console.log(linkElements);
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === '/') {
+      //relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        url.push(urlObj.href);
+      } catch (e) {
+        console.log(`error with relative url: ${e.message}`);
+      }
+    } else {
+      //absolute
+      try {
+        const urlObj = new URL(linkElement.href);
+        url.push(urlObj.href);
+      } catch (e) {
+        console.log(`error with absolute url: ${e.message}`);
+      }
+    }
+  }
+  return url;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const path = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,4 +38,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
